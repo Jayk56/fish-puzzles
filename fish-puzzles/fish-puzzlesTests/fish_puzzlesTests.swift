d36be893a3f72235aa40628d0ab6500c1d99ab27
Jay Kerschner <jay.kerschner@outlook.com>
2025-08-16 13:27:47 -0700

    project init xcode

---
+++ fish-puzzles/fish-puzzlesTests/fish_puzzlesTests.swift
@@ -0,0 +1,17 @@
+//
+//  fish_puzzlesTests.swift
+//  fish-puzzlesTests
+//
+//  Created by Jay Kerschner on 8/16/25.
+//
+
+import Testing
+@testable import fish_puzzles
+
+struct fish_puzzlesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
